a3eeb1eeab4c5ec72c0ad3b944caaf56282b1e71
dev@erxes.io <dev@erxes.io>
2026-07-02 15:19:03 +0800

    clean up

--- src/graphql/tour/mutations/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./orders";
-// Payment operations are shared — import from ecommerce:
-// import { INVOICE_CREATE, INVOICES_CHECK, PAYMENT_TRANSACTIONS_ADD } from "@/graphql/ecommerce/mutations/payment";
--- src/graphql/tour/queries/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./tours";
-export * from "./categories";
-export * from "./itinerary";
-export * from "./orders";
